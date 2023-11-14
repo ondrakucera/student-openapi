866148960d0bbf8e60b8639946f3cb97a8287613
Ondra Kučera <ondrej.kucera@centrum.cz>
2023-11-14 10:15:11 +0100

    Implement all operations

--- students-server-restapi-node/services/DefaultService.js
+++ students-server-restapi-node/services/DefaultService.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
 
+const CodebookItem = require('../domain/codebook');
+const Student = require('../domain/student');
+const studentDao = require('../dao/student-dao');
+
+const codebooks = createCodebooks();
+persistSampleStudents();
+
 /**
 * Deletes a student
 * Deletes a `Student`.
@@ -11,14 +18,10 @@ const Service = require('./Service');
 const deleteStudent = ({ studentId }) => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        studentId,
-      }));
+      studentDao.deleteById(studentId);
+      resolve({code: 204});
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+        reject(Service.rejectResponse('Not found', 404));
     }
   },
 );
@@ -32,14 +35,10 @@ const deleteStudent = ({ studentId }) => new Promise(
 const getCodebookItems = ({ codebookCode }) => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        codebookCode,
-      }));
+      const codebook = codebooks[codebookCode] ?? [];
+      resolve(Service.successResponse(codebook));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(Service.rejectResponse(e.message));
     }
   },
 );
@@ -53,14 +52,14 @@ const getCodebookItems = ({ codebookCode }) => new Promise(
 const getStudent = ({ studentId }) => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        studentId,
-      }));
+      const student = studentDao.findById(studentId);
+      if (student) {
+        resolve(Service.successResponse(student));
+      } else {
+        reject(Service.rejectResponse('Not found', 404));
+      }
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(Service.rejectResponse(e.message));
     }
   },
 );
@@ -73,13 +72,9 @@ const getStudent = ({ studentId }) => new Promise(
 const getStudents = () => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(Service.successResponse(studentDao.findAll()));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(Service.rejectResponse(e.message));
     }
   },
 );
@@ -93,14 +88,12 @@ const getStudents = () => new Promise(
 const postStudent = ({ student }) => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        student,
-      }));
+      const createdStudent = studentDao.save(
+        new Student(null, student.firstName, student.lastName, student.gender, student.house, student.year)
+      );
+      resolve(Service.successResponse(createdStudent.id, 200));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(Service.rejectResponse(e.message));
     }
   },
 );
@@ -115,18 +108,55 @@ const postStudent = ({ student }) => new Promise(
 const putStudent = ({ studentId, student }) => new Promise(
   async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        studentId,
-        student,
-      }));
+      studentDao.save(
+        new Student(
+          Number(studentId),
+          student.firstName,
+          student.lastName,
+          student.gender,
+          student.house,
+          student.year
+        )
+      );
+      resolve({code: 204});
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(Service.rejectResponse('Not found', 404));
     }
   },
 );
+
+function persistSampleStudents() {
+  studentDao.save(new Student(null, "Harry", "Potter", "M", "GRYFFINDOR", "2"));
+  studentDao.save(new Student(null, "Hermione", "Granger", "F", "GRYFFINDOR", "2"));
+  studentDao.save(new Student(null, "Ron", "Weasley", "M", "GRYFFINDOR", "2"));
+  studentDao.save(new Student(null, "Ginny", "Weasley", "F", "GRYFFINDOR", "1"));
+  studentDao.save(new Student(null, "Luna", "Lovegood", "F", "RAVENCLAW", "1"));
+  studentDao.save(new Student(null, "Draco", "Malfoy", "M", "SLYTHERIN", "2"));
+}
+
+function createCodebooks() {
+  return {
+    GENDER: [
+      new CodebookItem("M", { cs: "Muž", en: "Male" }, 1),
+      new CodebookItem("F", { cs: "Žena", en: "Female" }, 2),
+    ],
+    HOUSE: [
+      new CodebookItem("GRYFFINDOR", { cs: "Nebelvír", en: "Gryffindor" }, 1),
+      new CodebookItem("HUFFLEPUFF", { cs: "Mrzimor", en: "Hufflepuff" }, 2),
+      new CodebookItem("RAVENCLAW", { cs: "Havraspár", en: "Ravenclaw" }, 3),
+      new CodebookItem("SLYTHERIN", { cs: "Zmijozel", en: "Slytherin" }, 4),
+    ],
+    YEAR: [
+      new CodebookItem("1", { cs: "První", en: "First" }, 1),
+      new CodebookItem("2", { cs: "Druhý", en: "Second" }, 2),
+      new CodebookItem("3", { cs: "Třetí", en: "Third" }, 2),
+      new CodebookItem("4", { cs: "Čtvrtý", en: "Fourth" }, 2),
+      new CodebookItem("5", { cs: "Pátý", en: "Fifth" }, 2),
+      new CodebookItem("6", { cs: "Šestý", en: "Sixth" }, 2),
+      new CodebookItem("7", { cs: "Sedmý", en: "Seventh" }, 2),
+    ],
+  };
+}
 
 module.exports = {
   deleteStudent,
